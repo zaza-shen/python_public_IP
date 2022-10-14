e6751cc48208e03fc7f79a238e8f473147725318
zaza-shen <shengunyuzazashen@hotmail.com>
2022-10-13 19:59:16 -0500

    change_user_controller

--- flask_app/controllers/user_control.py
+++ flask_app/controllers/user_control.py
@@ -3,6 +3,8 @@ from flask import render_template,redirect,request,session,flash
 from flask_app.models.users import User
 from flask_app.models.shows import Show
 from flask_app.controllers import shows_control
+from flask_bcrypt import Bcrypt        
+bcrypt = Bcrypt(app)
 
 
 @app.route("/session_clear")
